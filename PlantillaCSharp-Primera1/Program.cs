1638ecf334610c19bbe58c68c2095e9d5ca2e8cf
Cristo Doniz <cristojosedm@gmail.com>
2022-12-01 14:36:35 +0100

    Agregar archivos de proyecto.

---
+++ PlantillaCSharp-Primera1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Ejercicio
+{
+    internal class Ejercicio
+    {
+        static void Main(String[] args)
+        {
+            int sumar;
+            int multiplicar;
+            int dividir;
+            int restar;
+            Console.WriteLine("Que desea hacer? \n 1 para sumar \n 2 para multiplicar \n 3 para dividir \n 4 para restar");
+        }
+    }
+}
